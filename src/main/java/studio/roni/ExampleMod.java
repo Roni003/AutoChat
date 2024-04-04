a2264c108cb2ea34bb56e91e6ce7dbb16d1a79b6
Roni003 <ronikazmirci003@gmail.com>
2024-04-04 15:30:10 +0100

    init

--- src/main/java/studio/roni/ExampleMod.java
+++ src/main/java/studio/roni/ExampleMod.java
@@ -1,4 +1,4 @@
-package studio.dreamys;
+package studio.roni;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "autopit";
     public static final String VERSION = "1.0";
     
     @Mod.EventHandler
